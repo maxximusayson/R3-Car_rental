565d2dcdffa35fcbdd627b3e6739c99e7be30479
Miles Gabriel Abude <invcrown72@gmail.com>
2024-11-11 01:03:27 +0800

    Update index2.blade.php

--- resources/views/index2.blade.php
+++ resources/views/index2.blade.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GPS Device Data - GPS02</title>
+    <title>GPS Device Data - GPS2</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCDg7pLs7iesp74vQ-KSEjnFJW3BKhVq7k"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
